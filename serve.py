d00a8533c7c662a2d6faa707683c439eef06e4e0
Kritika yadav <m22ph201@iitj.ac.in>
2025-11-24 00:52:22 +0530

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+from langserve import add_routes # this will help in creating apis
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.getenv('GROQ_API_KEY')
+# Langchain is creating integration with all kind of LLM Models
+model = ChatGroq(model='openai/gpt-oss-20b',groq_api_key=groq_api_key)
+
+system_template = 'Translate the following into {language}:'
+prompt=ChatPromptTemplate.from_messages([
+
+    ('system',system_template),
+     ('user','{text}')
+     
+     ])
+
+parser = StrOutputParser()
+ 
+ ## create chain
+chain = prompt|model|parser
+
+## app definition
+app = FastAPI(title = 'Langchain Server',
+              version ='1.0',
+              description = 'A simple API serever using Langchain runnable interface'
+)
+
+### Adding chain routes 
+add_routes(
+    app,
+    chain,
+    path='/chain'
+)
+
+if __name__=='__main__':
+    import uvicorn
+    uvicorn.run(app,host='127.0.0.1',port=8001)
+
+
